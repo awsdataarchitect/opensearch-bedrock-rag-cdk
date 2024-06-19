d034c274b9beda1e1a09a671754631b82de6c8d7
awsdataarchitect <velsovivek@gmail.com>
2024-06-19 18:48:20 -0400

    Initial commit

---
+++ lib/opensearch-bedrock-rag-cdk-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class OpensearchBedrockRagCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'OpensearchBedrockRagCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
