3e038f822ff8aeeb0efb2326e1c3cf1c1c63883a
awsdataarchitect <velsovivek@gmail.com>
2024-07-24 00:06:52 -0400

    added eks platform

--- bin/opensearch-bedrock-rag-cdk.ts
+++ bin/opensearch-bedrock-rag-cdk.ts
@@ -3,13 +3,57 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { OpensearchBedrockRagCdkStack } from '../lib/opensearch-bedrock-rag-cdk-stack';
 import { EcsFargateCdkStack } from '../lib/ecs-fargate-cdk-stack';
+import { EksClusterStack } from '../lib/eks-cluster-cdk-stack';
+import * as path from 'path';
+import * as fs from 'fs';
+
+// Read configuration from config.ini file
+const configPath = path.resolve(__dirname, '../config.ini');
+const configContent = fs.readFileSync(configPath, 'utf-8');
+const config: Record<string, string> = {};
+configContent.split('\n').forEach((line) => {
+  const [key, value] = line.split('=');
+  if (key && value) {
+    config[key.trim()] = value.trim();
+  }
+});
+
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+}
 
 const app = new cdk.App();
+
+const domainName = config['domainName'];
+const hostedZoneId = config['hostedZoneId'];
+const targetPlatform = config['targetPlatform'];
+
 const openSearchStack = new OpensearchBedrockRagCdkStack(app, 'OpensearchBedrockRagCdkStack', {
+  env: env,
 });
 
-new EcsFargateCdkStack(app, 'EcsFargateCdkStack', {
+const props = {
   OpenSearchEndpoint: openSearchStack.OpenSearchEndpoint,
-  VectorFieldName:    openSearchStack.VectorFieldName,
-  VectorIndexName:    openSearchStack.VectorIndexName
-});
+  VectorIndexName: openSearchStack.VectorIndexName,
+  VectorFieldName: openSearchStack.VectorFieldName,
+  bedrockPolicy: openSearchStack.bedrockPolicy,
+  openSearchPolicy: openSearchStack.openSearchPolicy,
+  sqs_queue_url: openSearchStack.sqs_queue_url,
+  domainName: domainName,
+  hostedZoneId: hostedZoneId,
+  env: env,
+}
+
+if (targetPlatform === 'ecs') {
+  new EcsFargateCdkStack(app, 'EcsFargateCdkStack', {
+    ...props
+  });
+
+} else if (targetPlatform === 'eks') {
+  new EksClusterStack(app, 'EksClusterStack', {
+    ...props
+  });
+
+
+}
